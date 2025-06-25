c5a36c73b6937b62e6ec6f1a996e858ea2bf541e
Ali Rafiei <alirafieiweb@gmail.com>
2025-06-25 19:15:26 +0330

    Ass Sqrt.java

---
+++ 002- Install JDK/Sqrt.java
@@ -0,0 +1,7 @@
+public class Sqrt {
+    public static void main(String[] args) {
+        double number = 9;
+        double result = Math.sqrt(number);
+        System.out.println("The square root of " + number + " is " + result);
+    }
+}
