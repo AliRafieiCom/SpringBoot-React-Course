6dc68049b94b0b673c705bd9bad4192d25408436
Ali Rafiei <alirafieiweb@gmail.com>
2025-07-18 09:22:48 +0330

    Main.java

---
+++ 004- Gradle/WithoutGradle/Main.java
@@ -0,0 +1,20 @@
+import com.google.gson.Gson;
+
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person("Ali", 32);
+        Gson gson = new Gson();
+        String json = gson.toJson(person);
+        System.out.println(json);
+    }
+}
+
+class Person {
+    String name;
+    int age;
+
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
